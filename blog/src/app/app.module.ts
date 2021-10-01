974a34228288546ed9c4b81fb56ef86b16e0c641
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-10-02 04:26:57 +0530

    Components and Important Point

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { UserListComponent } from './user-list/user-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent
+    UserListComponent,
+
   ],
   imports: [
     BrowserModule
